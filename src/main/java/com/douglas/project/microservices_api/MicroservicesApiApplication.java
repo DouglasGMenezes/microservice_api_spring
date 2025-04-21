744e783f5aac588953db500e8389e1c9ec2d5c1d
Douglas Menezes <douglasgm@MacBook-Air-de-Douglas.local>
2025-04-21 17:32:27 -0300

    new commit

---
+++ src/main/java/com/douglas/project/microservices_api/MicroservicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.project.microservices_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/project/microservices_api/MicroservicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.project.microservices_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
